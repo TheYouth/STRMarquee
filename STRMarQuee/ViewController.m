886d7cd5a97b41eb597c28b0a61460582b44c543
renxingxing <renxingxing@renxingxingdeMac.local>
2017-06-21 21:51:58 +0800

    Gif

--- STRMarQuee/ViewController.m
+++ STRMarQuee/ViewController.m
@@ -20,15 +20,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    STRAutoScrollLabel *autoLabel = [[STRAutoScrollLabel alloc] initWithFrame:CGRectMake(10, 30, 200, 40)];
-    autoLabel.text = @"跑马灯效果！哈哈哈哈哈哈，实现了，看看效果好不好";
+
+    STRAutoScrollLabel *autoLabel = [[STRAutoScrollLabel alloc] initWithFrame:CGRectMake(10, 30, self.view.frame.size.width, 40)];
+    autoLabel.text = @"跑马灯效果！哈哈哈哈哈哈，实现了，看看效跑马灯效果！哈哈哈哈哈哈，实现了，看看效果好不好果好不好";
     //color
     autoLabel.textColor = [UIColor redColor];
-    //根据实际情况，添加速度及之间间距
-//    autoLabel.speed = 70;
-//    autoLabel.labelBetweenGap = 10;
-    
     [self.view addSubview:autoLabel];
+    
+    //根据实际情况，添加速度及之间间距
+    //    autoLabel.speed = 70;
+    //    autoLabel.labelBetweenGap = 10;
     
     
 }
